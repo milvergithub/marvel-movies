0c9451a0bb90b26bb662ac2133ee20154c6b19ba
Milver Flores Acevedo <mflores@rootstack.com>
2021-12-25 23:06:01 -0400

    MM-03: heroku deploy configuration

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const serveStatic = require('serve-static');
+const path = require('path');
+
+// eslint-disable-next-line no-undef
+app = express();
+// eslint-disable-next-line no-undef
+app.use(serveStatic(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 3000;
+// eslint-disable-next-line no-undef
+app.listen(port);
